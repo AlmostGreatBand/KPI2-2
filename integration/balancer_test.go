d9ef9419347c11e9c6f541a8304a0a5caff735b0
NeProgramist <zaskozhenya@gmail.com>
2021-05-15 14:52:32 +0300

    feat(lab2-2): finish integration test&bood fixes

--- integration/balancer_test.go
+++ integration/balancer_test.go
@@ -2,6 +2,7 @@ package integration
 
 import (
 	"fmt"
+	"github.com/stretchr/testify/assert"
 	"net/http"
 	"testing"
 	"time"
@@ -10,15 +11,46 @@ import (
 const baseAddress = "http://balancer:8090"
 
 var client = http.Client{
-	Timeout: 3 * time.Second,
+	Timeout: 10 * time.Second,
 }
 
 func TestBalancer(t *testing.T) {
-	resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
-	if err != nil {
-		t.Error(err)
+	/*
+		there is no proper way to test this algorithm(with the least connections)
+		I've tried manually test this algorithm with 900 ms client delay, and 3 s server delay
+		check this here
+		https://docs.google.com/spreadsheets/d/1ZjhszAWXc9_hmWr7m9g-Z32HDHwIvSzkUklp81oPRUg/edit?usp=sharing
+	 */
+
+	serverUrls := []string {
+		"server1:8080",
+		"server2:8080",
+		"server3:8080",
 	}
-	t.Logf("response from [%s]", resp.Header.Get("lb-from"))
+
+	expectedResults := []int32 { 0, 1, 2, 0, 0, 1, 2, 0, 0, 1 }
+
+	serverNames := make(chan string, len(expectedResults))
+
+	for range expectedResults {
+		go func() {
+			resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
+
+			if resp == nil || resp.StatusCode != http.StatusOK || err != nil {
+				t.Logf("error %s", err)
+			}
+
+			server := resp.Header.Get("lb-from")
+			t.Logf("response from [%s]", server)
+			serverNames <- server
+		}()
+		time.Sleep(900 * time.Millisecond)
+	}
+
+	for _, i := range expectedResults {
+		server := <- serverNames
+		assert.Equal(t, server, serverUrls[i])
+	}
 }
 
 func BenchmarkBalancer(b *testing.B) {
